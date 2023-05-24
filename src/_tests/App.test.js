74ed7882deaaa013624499780eaa064b5e1c768c
Guppy <guppy@Guppys-MacBook-Air.local>
2023-05-24 21:28:23 +0100

    fix: updated react-testing/user-event library, as v14 fixes known issue regarding react router not navigating to new page inside test.

--- src/_tests/App.test.js
+++ src/_tests/App.test.js
@@ -1,9 +1,11 @@
 import userEvent from "@testing-library/user-event";
 import Header from "../components/Header";
+import SignInSide from "../pages/LogIn";
 import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 
 describe("header", () => {
-  test("Detect darkmodeswitch has rendered", async () => {
+  test("Detect darkmodeswitch has rendered with app bar", async () => {
     render(<Header />);
   });
   test("Detect that the darkmode switch functions when clicked", async () => {
@@ -14,5 +16,26 @@ describe("header", () => {
     expect(avatarBox).toHaveStyle("backgroundColor: rgb(189 189 189 )");
     await userEvent.click(toggleDarkModeSwitch);
     expect(avatarBox).toHaveStyle("backgroundColor: rgb(117 117 117 )");
+  });
+});
+describe("Login", () => {
+  test("Detect the login page", async () => {
+    render(
+      <BrowserRouter>
+        <SignInSide />
+      </BrowserRouter>
+    );
+  });
+  test("Detect the google Login button on the login page", async () => {
+    const user = userEvent.setup();
+    render(
+      <BrowserRouter>
+        <SignInSide />
+      </BrowserRouter>
+    );
+    await user.click(
+      screen.getByRole("button", { name: /Sign in with Google/i })
+    );
+    expect(screen.getByRole("button", { name: /Sign in with Google/i }));
   });
 });
